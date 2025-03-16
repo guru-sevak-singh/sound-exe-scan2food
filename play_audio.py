e0254af8b9e2998ed65962d6bd987c0391317300
guru-sevak-singh <116969988+guru-sevak-singh@users.noreply.github.com>
2025-03-16 15:08:40 +0530

    create exe

--- play_audio.py
+++ play_audio.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-
+import wget
 
 def play_audio(file_path):
     pygame.mixer.init()
@@ -22,8 +22,9 @@ def download_audios():
     if os.path.exists(audio_path):
         pass
     else:
+        download_url = "https://guru-sevak-singh.github.io/sound-exe-scan2food/order_received.mp3"
         # download the audio.
-        pass
+        wget.download(download_url, os.path.join(os.getcwd(), 'order_received.mp3'))
 
     
     audio_path = os.path.join(os.getcwd(), 'pending_order.mp3')
@@ -31,5 +32,8 @@ def download_audios():
         pass
     else:
         # Download the audio.
-        pass
+        download_url = "https://guru-sevak-singh.github.io/sound-exe-scan2food/pending_order.mp3"
+        wget.download(download_url, os.path.join(os.getcwd(), 'pending_order.mp3'))
 
+
+download_audios()
